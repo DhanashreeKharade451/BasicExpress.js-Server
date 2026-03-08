71aec41afcb6a9b43746205203f4fd513f87702f
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-08 08:38:18 -0400

    route handler

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 import express from 'express'
+
+//built-in path module
+import path from 'path'
+
+//instance of an Express application.
+const app = express();
+
+// port to run the server
+const port = 3000
+
+ //route handler for GET requests to the root URL (/).
+ 
