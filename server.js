d760b3b31a68066d4ebc59ccbd59ad03a762d5dc
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-08 16:26:26 -0400

    sending index.html file

--- server.js
+++ server.js
@@ -9,5 +9,19 @@ const app = express();
 // port to run the server
 const port = 3000
 
- //route handler for GET requests to the root URL (/).
+//Create a route handler for GET requests to the root URL (/).
+//  When this route is requested, it should send the index.html file
+//  from your public directory.
+app.use(express.static("public"));
+
+
+//Create another route handler for GET requests to /contact. 
+// This should send the contact.html file.
+
+app.use(express.static(path.join(__dirname,'contact')))
+
+ //listen on  port
+ app.listen(port,() => {
+    console.log('server listening on port' +port)
+ });
  
